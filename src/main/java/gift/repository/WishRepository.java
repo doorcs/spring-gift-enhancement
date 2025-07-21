65924e2b23471a5aa4be2b9fa9fad4c64071b0c2
doorcs <itsdoorcs@gmail.com>
2025-07-21 17:59:32 +0900

    refactor: jpql 메서드의 count 쿼리 분리

--- src/main/java/gift/repository/WishRepository.java
+++ src/main/java/gift/repository/WishRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -14,10 +13,14 @@ import gift.dto.WishItem;
 @Repository
 public interface WishRepository extends JpaRepository<Wish, Long> {
 
-    @Query("""
+    @Query(value = """
         SELECT new gift.dto.WishItem(w.id, p.id, p.price.price, p.productName.productName, p.imageUrl.imageUrl, w.quantity.quantity)
         FROM Wish AS w
         JOIN w.product AS p
+        WHERE w.member.id = :memberId
+        """, countQuery = """
+        SELECT COUNT(w)
+        FROM Wish AS w
         WHERE w.member.id = :memberId
         """)
     List<WishItem> findAllProductByMemberId(Long memberId, Pageable pageable);
